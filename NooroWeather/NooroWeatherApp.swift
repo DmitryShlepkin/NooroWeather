cd83797d6aef125f01b5931e46fed83b7ef5f232
Dmitry Shlepkin <acidl33t@gmail.com>
2025-01-29 11:08:54 -0500

    Initial Commit

---
+++ NooroWeather/NooroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherApp.swift
+//  NooroWeather
+//
+//  Created by Dmitry Shlepkin on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NooroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NooroWeatherTests/NooroWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherTests.swift
+//  NooroWeatherTests
+//
+//  Created by Dmitry Shlepkin on 1/29/25.
+//
+
+import Testing
+@testable import NooroWeather
+
+struct NooroWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
